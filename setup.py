768530dc47f8019882f9948a9133716f3d90369d
Yoonsoo Kim <laptise@live.jp>
2023-12-13 19:28:01 +0900

    bump v

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def requirements_from_file(file_name):
 setup(
     name = "mlvault", 
     packages = find_packages(),
-    version="0.0.20",
+    version="0.0.21",
     author="MLVault",
     install_requires=requirements_from_file("requirements.txt"),
     entry_points={
